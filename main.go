f6df3946ce5409312033fe54997bdf22adc69039
Happiwolf <ameslexander@gmail.com>
2024-09-18 09:25:52 -0500

    Added HTTP handler for processing receipts and assigning points

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
 type Receipt struct {
-	Retailer string
-	Total    string
+	Retailer string `json:"retailer"`
+	Total    string `json:"total"`
+}
+
+var receiptsStore = make(map[string]int)
+
+func processReceipt(w http.ResponseWriter, r *http.Request) {
+	var receipt Receipt
+	err := json.NewDecoder(r.Body).Decode(&receipt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	receiptID := uuid.New().String()
+	receiptsStore[receiptID] = 100
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"id": receiptID})
 }
 
 func main() {
+	http.HandleFunc("/receipts/process", processReceipt)
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
